5d806b5570e3b74be24a3b1b43c815c3797d7e7a
myllymaj <myllymaj@arcada.fi>
2023-10-03 14:43:03 +0300

    websocket connection working

--- server.js
+++ server.js
@@ -1,63 +1,92 @@
+//chatgpt
+//chatgpt
+//chatgpt
+
+
 const WebSocket = require('ws')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 const wss = new WebSocket.Server({ port: PORT })
 
 // Set: datatyp "med bara nycklar", Wikipedia: Unlike most other collection types, rather than retrieving a specific element from a set, one typically tests a value for membership in a set. 
-const clients = new Set()
-
+const boards = {};
+const boardMessages = {};
 // URL example: ws://my-server?token=my-secret-token
 wss.on('connection', (ws, req) => {
     
+
+
+
     // Check valid token (set token in .env as WS_TOKEN=my-secret-token )
     const urlParams = new URLSearchParams(req.url.slice(1));
-    if (urlParams.get('token') !== process.env.WS_TOKEN) {
-        console.log('Invalid token: ' + urlParams.get('token'));
-        ws.send(JSON.stringify({
-            type: 'error',
-            msg: 'ERROR: Invalid token.'
-        }));
-        ws.close();
-    }
+    const token = urlParams.get('token');
+    const boardId = urlParams.get('board_id');
 
-    // Spara connectionen i vårt client-Set:
-    if (!clients.has(ws)) {
-        ws.createdAt = new Date()
-        clients.add(ws)
-    }
-    console.log('Client connected:', req.headers['sec-websocket-key'], 
-        'client count:', clients.size, ws);
-
-    ws.on('message', (rawMessage) => {
-
-        ws.lastMessage = new Date()
     
-        // Vi konverterar vår råa JSON till ett objekt
-        const message = JSON.parse(rawMessage.toString())
-
-        message.clientId = req.headers['sec-websocket-key']
-
-        console.log('Received message:', message)
-
-        clients.forEach(client => {
-
-            // Skicka inte till vår egen klient (ws)
-            if (client === ws) return
-
-            client.send(JSON.stringify({
-                type: 'paste',
-                text: message.text
+    // Verify the JWT token here
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            console.log('Invalid token:', err);
+            ws.send(JSON.stringify({
+                type: 'error',
+                msg: 'ERROR: Invalid token.'
             }));
-        })
+            ws.close();
+        } else {
+            // Token is valid, you can access its contents in the 'decoded' variable
+            console.log('Valid token for user:', decoded.username);
 
+            if (!boards[boardId]) {
+                boards[boardId] = new Set();
+            }
+            if (boardMessages[boardId] && boardMessages[boardId].length > 0) {
+                boardMessages[boardId].forEach(message => {
+                    ws.send(JSON.stringify({
+                        type: 'paste',
+                        text: message.text
+                    }));
+                });
+            }
+            boards[boardId].add(ws);
+
+            console.log('Client connected:',
+                'client count:', boards[boardId].size);
+
+                ws.on('message', (rawMessage) => {
+
+                    ws.lastMessage = new Date()
+                
+                    // Vi konverterar vår råa JSON till ett objekt
+                    const message = JSON.parse(rawMessage.toString())
+            
+                    message.clientId = req.headers['sec-websocket-key']
+            
+                    console.log('Received message:', message)
+                    if (!boardMessages[boardId]) {
+                        boardMessages[boardId] = [];
+                    }
+                    boardMessages[boardId].push(message);
+                    
+                    boards[boardId].forEach(client => {
+            
+                        // Skicka inte till vår egen klient (ws)
+                        if (client === ws) return
+            
+                        client.send(JSON.stringify({
+                            type: 'paste',
+                            text: message.text
+                        }));
+                    })
+            
+                });
+            
+
+            ws.on('close', () => {
+                console.log('Client disconnected');
+                boards[boardId].delete(ws);
+            });
+        }
     });
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
-    
-
 });
-
-
